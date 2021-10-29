6a2a10f18ebec7b7405f217f0a78bd809cd0829a
Edward Paler <paler.edward@gmail.com>
2021-10-29 20:24:59 +0800

    Add activity code

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -7,3 +7,88 @@ module.exports.getAllTasks = () => {
 		return result;
 	})
 }
+
+//Controller function for creating a task
+module.exports.createTask = (requestBody) => {
+	//Creates a task object based on the Mongoose model "Task"
+	let newTask = new Task({
+		//Sets the "name" property with the value received from the client/Postman
+		name: requestBody.name,
+	})
+
+	return newTask.save().then((task, error) => {
+		if(error){
+			console.log(error);
+			return false;
+		}
+		else{
+			return task;
+		}
+	})
+}
+
+//Controller function for deleting a task
+module.exports.deleteTask = (taskId) => {
+	return Task.findByIdAndRemove(taskId).then((removedTask, err) => {
+		if(err){
+			console.log(err);
+			return false;
+		}
+		else{
+			return removedTask;
+		}
+	})
+}
+
+
+//Controller function for updating a task
+module.exports.updateTask = (taskId, newContent) => {
+	return Task.findById(taskId).then((result, error) => {
+		if(error){
+			console.log(error);
+			return false;
+		}
+
+		result.name = newContent.name;
+
+		return result.save().then((updatedTask, saveErr) => {
+			if (saveErr){
+				console.log(saveErr);
+				return false;
+			}
+			else{
+				return updatedTask;
+			}
+		})
+	})
+}
+
+
+//2. Create a controller function for retrieving a specific task.
+module.exports.getOneTask = (taskId) => {
+	return Task.findById(taskId).then((result) => {
+		return result;
+	})
+}
+
+//6. Create a controller function for changing the status of a task to "complete".
+module.exports.completeTask = (taskId) => {
+	return Task.findById(taskId).then((result, error) => {
+		if(error){
+			console.log(error);
+			return false;
+		}
+
+		result.status = "complete";
+
+		return result.save().then((completeTask, saveErr) => {
+			if (saveErr){
+				console.log(saveErr);
+				return false;
+			}
+			else{
+				return completeTask;
+			}
+		})
+	})
+}
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -13,4 +13,33 @@ router.get("/", (req, res) => {
 		resultFromController));
 })
 
+//Route to create a new task
+router.post("/", (req, res) => {
+	taskController.createTask(req.body).then(resultFromController => res.send(resultFromController));
+})
+
+//Route for deleting a task
+//localhost:3001/tasks/091239s0df990
+router.delete("/:id", (req,res) => {
+	taskController.deleteTask(req.params.id).then(resultFromController => res.send(resultFromController));
+})
+
+//Route for updating a task
+router.put("/:id", (req,res) => {
+	taskController.updateTask(req.params.id, req.body).then(resultFromController => res.send(resultFromController));
+})
+
+//1. Create a route for getting a specific task.
+router.get("/:id", (req, res) => {
+	taskController.getOneTask(req.params.id).then(resultFromController => res.send(
+		resultFromController));
+})
+
+//5. Create a route for changing the status of a task to "complete".
+router.put("/:id/complete", (req,res) => {
+	taskController.completeTask(req.params.id, req.body).then(resultFromController => res.send(resultFromController));
+})
+
+
+
 module.exports = router;
